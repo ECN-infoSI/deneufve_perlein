1332b137a933f00b7c5c4d94e4eb2f078956818c
Benjamin Deneufve <benjamin.deneufve@eleves.ec-nantes.fr>
2024-09-20 17:04:27 +0200

    maj du Main et opti de mondeAlea

--- WOE_poo/src/org/centrale/objet/WoE/World.java
+++ WOE_poo/src/org/centrale/objet/WoE/World.java
@@ -14,25 +14,46 @@ public class World {
     public Paysan peon ; 
     public Lapin bugs ; 
     public int taille ;
+
+    public World(Archer hog_rider, Paysan peon, Lapin bugs, int taille) {
+        this.hog_rider = hog_rider;
+        this.peon = peon;
+        this.bugs = bugs;
+        this.taille = taille;
+    }
+
+    public World() {
+        this.hog_rider = new Archer();
+        this.peon = new Paysan();
+        this.bugs = new Lapin();
+        this.taille = 1000;
+    }
     
-    public void creeMondeAlea(){
-             
-        Point2D[] l_pos = new Point2D[3] ;
-        
-        Random gen = new Random() ; 
-        
-        l_pos[0] = new Point2D(gen.nextInt(taille), gen.nextInt(taille));
-        l_pos[1] = new Point2D(gen.nextInt(taille), gen.nextInt(taille));
-        l_pos[2] = new Point2D(gen.nextInt(taille), gen.nextInt(taille));
-        
-        while (l_pos[0].equal(l_pos[1]) || l_pos[0].equal(l_pos[2]) || l_pos[2].equal(l_pos[1])){
-            l_pos[0].setPosition(gen.nextInt(taille), gen.nextInt(taille));
-            l_pos[1].setPosition(gen.nextInt(taille), gen.nextInt(taille));
-            l_pos[2].setPosition(gen.nextInt(taille), gen.nextInt(taille));
-        } 
-        
-        hog_rider.setPos(l_pos[0]) ; 
-        peon.setPos(l_pos[1]) ; 
-        bugs.setPos(l_pos[2]) ; 
+    public void creerMondeAlea() {
+        Random random = new Random();
+        Point2D[] positions = new Point2D[3];
+        boolean estUnique;
+        // Génération des positions pour chaque entité
+        for (int i = 0; i < positions.length; i++) {
+            estUnique = false;
+            // Continue jusqu'à obtenir une position unique
+            while (!estUnique) {
+                Point2D newPosition = new Point2D(random.nextInt(taille), random.nextInt(taille));
+                estUnique = true;
+                // Vérifie que la position n'est pas déjà utilisée
+                for (int j = 0; j < i; j++) {
+                    if (newPosition.equals(positions[j])) {
+                        estUnique = false;
+                        break;
+                    }
+                }
+                if (estUnique) {
+                    positions[i] = newPosition;
+                }
+            }
+        }
+        hog_rider.setPos(positions[0]);
+        peon.setPos(positions[1]);
+        bugs.setPos(positions[2]);
     }
 }
