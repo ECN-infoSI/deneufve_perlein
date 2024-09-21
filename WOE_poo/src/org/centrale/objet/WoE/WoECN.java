0371ef0fab0112c82ea6ea2425b9c5013def88aa
Benjamin Deneufve <benjamin.deneufve@eleves.ec-nantes.fr>
2024-09-21 08:10:15 +0200

    ajout des classes Objet, PotionSoin, Epee

--- WOE_poo/src/org/centrale/objet/WoE/WoECN.java
+++ WOE_poo/src/org/centrale/objet/WoE/WoECN.java
@@ -9,8 +9,41 @@ package org.centrale.objet.WoE;
  * @author benja
  */
 public class WoECN {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Archer hog_rider = new Archer(); 
+        Paysan peon = new Paysan(); 
+        Lapin bugs = new Lapin(); 
+        int taille = 1000;
+        World w =  new World(hog_rider, peon, bugs, taille);
+        w.creeMondeAlea();
+        
+        System.out.println("position des Mobs: ");
+        System.out.println("Hog_rider:");
+        hog_rider.getPos().affiche();
+        System.out.println("Peon:");
+        peon.getPos().affiche();
+        System.out.println("Buggs:");
+        bugs.getPos().affiche();
+        
+        // Déplacement des objets
+        hog_rider.deplace(taille);
+        peon.deplace(taille);
+        bugs.deplace(taille);
+        
+        System.out.println("position des Mobs:");
+        System.out.println("Hog_rider:");
+        hog_rider.getPos().affiche();
+        System.out.println("Peon:");
+        peon.getPos().affiche();
+        System.out.println("Buggs:");
+        bugs.getPos().affiche();
+        
+        //copie des objets
+        Archer guillaumeT;
+        guillaumeT = hog_rider;
+        hog_rider.deplace(taille);
+        //guillaumeT s'est aussi déplacé, il faut plutôt utiliser le constructeur de recopie
+        guillaumeT = new Archer(hog_rider);
     }
 }
