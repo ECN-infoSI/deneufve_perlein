729cee164ac01caa5d2f03dafee7168d92e5435d
Benjamin Deneufve <benjamin.deneufve@eleves.ec-nantes.fr>
2024-09-30 14:20:30 +0200

    ajout doc

--- WOE_poo/src/org/centrale/objet/WoE/Epee.java
+++ WOE_poo/src/org/centrale/objet/WoE/Epee.java
@@ -5,34 +5,64 @@
 package org.centrale.objet.WoE;
 
 /**
- *
- * @author ASUS
+ * Classe représentant une épée, un type d'objet dans le jeu.
+ * Hérite de la classe Objet.
  */
 public class Epee extends Objet {
+    /**
+     * Dégâts infligés par l'épée.
+     */
+    
     private int degats ; 
 
-    public Epee(String n, String s, int p, int d) {
-        super(n,s,p) ; 
+    /**
+     * Constructeur avec paramètres.
+     * 
+     * @param n Nom de l'objet.
+     * @param s Statut de l'objet.
+     * @param pds Poids de l'objet.
+     * @param d Dégâts de l'épée.
+     */
+    
+    public Epee(String n, String s, int pds, Point2D pos, int d) {
+        super(n,s,pds,pos) ; 
         this.degats = d ;
     }
     
+    /**
+     * Constructeur prenant un objet et définissant les dégâts.
+     * 
+     * @param o Objet à partir duquel l'épée est créée.
+     * @param d Dégâts de l'épée.
+     */
     public Epee(Objet o, int d) {
         super(o) ; 
         this.degats = d ; 
     }
     
+    /**
+     * Constructeur par défaut.
+     */
     public Epee() {
         super() ; 
         this.degats = 0 ;  
     }
 
+    /**
+     * Retourne les dégâts infligés par l'épée.
+     * 
+     * @return Dégâts de l'épée.
+     */
     public int getDegats() {
         return degats;
     }
 
+    /**
+     * Définit les dégâts de l'épée.
+     * 
+     * @param degats Dégâts à définir.
+     */
     public void setDegats(int degats) {
         this.degats = degats;
     }
-    
-    
 }
