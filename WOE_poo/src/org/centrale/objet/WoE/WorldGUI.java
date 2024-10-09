c705c9580758d22eb757d0337e437d19b041f02c
Benjamin Deneufve <benjamin.deneufve@eleves.ec-nantes.fr>
2024-10-09 21:36:01 +0200

    up interface

--- WOE_poo/src/org/centrale/objet/WoE/WorldGUI.java
+++ WOE_poo/src/org/centrale/objet/WoE/WorldGUI.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.LinkedList;
-import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.LinkedList;
 
 /**
  * Classe représentant l'interface graphique du monde du jeu.
@@ -27,13 +27,15 @@ public class WorldGUI extends JFrame {
         world = w;
 
         // Configuration de la fenêtre principale
-        setTitle("World of ECN 1.10");
-        setSize(800, 600);
+        setTitle("World of ECN 1.25");
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        setSize(screenSize.width, screenSize.height - 40);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         // Création du panneau de la carte du monde
         worldPanel = new JPanel();
-        worldPanel.setLayout(new GridLayout(world.getTaille(), world.getTaille()));
+        worldPanel.setLayout(new GridLayout(world.getTaille(), world.getTaille(), 1, 1));
 
         // Création du panneau d'information
         infoPanel = new JTextArea();
@@ -42,8 +44,8 @@ public class WorldGUI extends JFrame {
 
         // Ajouter les éléments à la fenêtre
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, worldPanel, new JScrollPane(infoPanel));
-        splitPane.setDividerLocation(600);
-        getContentPane().add(splitPane);
+        splitPane.setDividerLocation(1500);
+        getContentPane().add(splitPane, BorderLayout.CENTER);
 
         // Bouton pour afficher le monde
         JButton displayButton = new JButton("Afficher le Monde");
@@ -51,7 +53,7 @@ public class WorldGUI extends JFrame {
             afficherMonde(); // Afficher le monde quand le bouton est cliqué
         });
 
-        getContentPane().add(displayButton, BorderLayout.SOUTH);
+        getContentPane().add(displayButton, BorderLayout.NORTH);
     }
 
     /**
@@ -63,52 +65,75 @@ public class WorldGUI extends JFrame {
         LinkedList<Monstre> monstres = world.getMonstres();
         LinkedList<Objet> objets = world.getObjets();
 
+        // Charger les images avec redimensionnement
+        ImageIcon joueurIcon = new ImageIcon(new ImageIcon("C:/Users/benja/Documents/01_InfoSI/OBJET/deneufve_perlein/WOE_poo/joueur.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+        ImageIcon personnageIcon = new ImageIcon(new ImageIcon("C:/Users/benja/Documents/01_InfoSI/OBJET/deneufve_perlein/WOE_poo/personnage.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+        ImageIcon monstreIcon = new ImageIcon(new ImageIcon("C:/Users/benja/Documents/01_InfoSI/OBJET/deneufve_perlein/WOE_poo/monstre.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+        ImageIcon objetIcon = new ImageIcon(new ImageIcon("C:/Users/benja/Documents/01_InfoSI/OBJET/deneufve_perlein/WOE_poo/objet.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+        ImageIcon herbe = new ImageIcon(new ImageIcon("C:/Users/benja/Documents/01_InfoSI/OBJET/deneufve_perlein/WOE_poo/herbe.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+
         // Créer des boutons pour représenter chaque case de la grille
         for (int i = 0; i < world.getTaille(); i++) {
             for (int j = 0; j < world.getTaille(); j++) {
                 JButton button = new JButton();
+                button.setPreferredSize(new Dimension(50, 50)); // Taille préférée pour le bouton
+                button.setMinimumSize(new Dimension(50, 50)); // Taille minimale
+                button.setMaximumSize(new Dimension(50, 50)); // Taille maximale
+
                 Point2D point = new Point2D(i, j);
 
                 // Vérifier si un personnage, un monstre ou un objet est à cette position
                 boolean entityAdded = false;
-                
-                if (world.getPersoJoueur().getPos().equals(point)){
-                        button.setBackground(Color.GREEN); // Représente le joueur
-                        button.addActionListener(e -> afficherInfo(world.getPersoJoueur()));
+
+                // Si le joueur est sur cette case
+                if (world.getPersoJoueur().getPos().equals(point)) {
+                    button.setIcon(joueurIcon); // Image pour le joueur
+                    button.addActionListener(e -> afficherInfo(world.getPersoJoueur()));
+                    entityAdded = true;
                 }
-                
+
+                // Si ce n'est pas le joueur, vérifier les autres entités
                 if (!entityAdded) {
                     for (Personnage p : personnages) {
                         if (p.getPos().equals(point)) {
-                            button.setBackground(Color.BLUE); // Représente un personnage
+                            button.setIcon(personnageIcon); // Image pour un personnage
                             button.addActionListener(e -> afficherInfo(p));
                             entityAdded = true;
-
+                            break;
                         }
                     }
                 }
+
                 if (!entityAdded) {
                     for (Monstre m : monstres) {
                         if (m.getPos().equals(point)) {
-                            button.setBackground(Color.RED); // Représente un monstre
+                            button.setIcon(monstreIcon); // Image pour un monstre
                             button.addActionListener(e -> afficherInfo(m));
                             entityAdded = true;
-                            
+                            break;
                         }
                     }
                 }
+
                 if (!entityAdded) {
                     for (Objet o : objets) {
                         if (o.getPos().equals(point)) {
-                            button.setBackground(Color.YELLOW); // Représente un objet
+                            button.setIcon(objetIcon); // Image pour un objet
                             button.addActionListener(e -> afficherInfo(o));
                             entityAdded = true;
+                            break;
                         }
                     }
                 }
+
+                if (!entityAdded) {
+                    button.setIcon(herbe); // Image pour une case vide
+                }
+                // Ajouter le bouton au panneau du monde
                 worldPanel.add(button);
             }
         }
+
         worldPanel.revalidate(); // Rafraîchir le panneau
         worldPanel.repaint();    // Repeindre le panneau
     }
@@ -141,7 +166,7 @@ public class WorldGUI extends JFrame {
 
             // Récupérer le texte capturé dans la sortie console
             String info = baos.toString(); // Convertir le contenu capturé en chaîne
-            
+
             // Afficher les informations dans le panneau d'information
             infoPanel.setText(info); // Mettre à jour le JTextArea avec les informations
 
